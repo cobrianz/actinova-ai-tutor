9e247fa52b757199d06b07bde3ffd9e7da1241b4
cobrianz <briancheruiyot.h@gmail.com>
2026-03-14 14:20:07 +0300

    feat: setup next-sitemap for seo generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://actirova.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+}
